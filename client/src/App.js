e8e020f8253ae23bd6048bd10eb65d6ca7ce8113
eza <alfian.putra@solusi247.com>
2023-06-04 20:19:52 +0700

    add basic functionality (write and preview)

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import {marked} from 'marked';
+import { useState } from "react";
 
-function App() {
+
+export default function App() {
+  const [markdown, setMarkdown] = useState(`
+  #HELLO MARKDOWN !`);
+  function change(e){
+    setMarkdown(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+      <div className="inline-block">
+        <div className="app inline-block">
+          <textarea className="align-text-top ml-5 mr-15 h-[800px] w-[1150px] rounded-xl shadow border p-8 m-10" onChange={change} value={markdown}/>
+        </div>
+
+          <div
+            className="align-top h-[800px] w-[1150px] mr-5 inline-block inlinecontainer mx-auto bg-gray rounded-xl shadow border p-8 m-10"
+            dangerouslySetInnerHTML={{__html: marked(markdown)}}/>
+      </div>
+    </html>
   );
 }
-
-export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import react from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
